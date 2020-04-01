e5235abcc19d912190182116b0d66098e1e22f30
CloutNinja <54197644+CloutNinja@users.noreply.github.com>
2020-04-01 02:09:22 -0600

    Add files via upload

---
+++ L5B/L5C/cmake-build-debug/CMakeFiles/L5C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L5C.dir/main.cpp.obj"
+  "L5C.exe"
+  "L5C.exe.manifest"
+  "L5C.pdb"
+  "libL5C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L5C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
